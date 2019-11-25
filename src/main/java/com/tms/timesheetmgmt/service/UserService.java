1c2465e6559d201b9cda12e00182e939e6e1f16b
Arunkumar Mourougappane <arunkumar-mourougappane@users.noreply.github.com>
2019-11-25 17:47:19 -0600

    Updated database connections, Rebuild app on oraclejdk13 and added travis configurations

--- src/main/java/com/tms/timesheetmgmt/service/UserService.java
+++ src/main/java/com/tms/timesheetmgmt/service/UserService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Optional;
 
 @Service("userService")
 public class UserService {
