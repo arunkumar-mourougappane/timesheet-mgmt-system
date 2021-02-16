57c2b4091d5ec7cd048b983f473258329fb370fc
Arunkumar Mourougappane <amouroug@buffalo.edu>
2021-02-16 16:42:31 -0600

    Added junit tests for loading pages.

--- src/main/java/com/tms/Controller/LoginController.java
+++ src/main/java/com/tms/Controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.tms.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class LoginController
 {
-   @GetMapping(value="/")
+   @RequestMapping(value="/")
    public ModelAndView home(){
        ModelAndView modelAndView = new ModelAndView();
        modelAndView.setViewName("index");
--- src/test/java/com/tms/Application/TimesheetMgmtSystemApplicationTests.java
+++ src/test/java/com/tms/Application/TimesheetMgmtSystemApplicationTests.java
@@ -1,13 +1,36 @@
 package com.tms.Application;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-@SpringBootTest
+import com.tms.Controller.LoginController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TimesheetMgmtSystemApplicationTests {
 
+	@Autowired
+	private LoginController logincontroller;
+	
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 	@Test
 	void contextLoads() {
+		assertNotNull(logincontroller);
 	}
+
+	@Test
+    public void testForHomeRequestMapping() throws Exception {
+        assertTrue(this.restTemplate.getForObject("http://localhost:" + port + "/",String.class).contains("Timesheet Management System"));
+    }
 
 }
